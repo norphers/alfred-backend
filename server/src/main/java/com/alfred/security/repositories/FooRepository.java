4b1a639d704f6024771aec8178947b48cc703850
ocanades <ocanades@gmail.com>
2020-08-14 16:00:13 +0200

    delete all security --> rebuilt in future

--- server/src/main/java/com/alfred/security/repositories/FooRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alfred.security.repositories;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.alfred.security.models.Foo;
-
-public interface FooRepository extends PagingAndSortingRepository<Foo, Long> {
-
-}
